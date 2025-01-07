cfd8bcdca78deaf5631497a1a46e0a1749c89632
HoneySafonovets <kfs.boi@yandex.ru>
2025-01-08 02:09:02 +0300

    feat: add function generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -450,8 +450,17 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const newArray = [];
+  Array(len)
+    .fill()
+    .map((e, i) => {
+      if (e === 1) {
+        return newArray.push(1);
+      }
+      return newArray.push(2 * i + 1);
+    });
+  return newArray;
 }
 
 /**

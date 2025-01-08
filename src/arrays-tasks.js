89b36084893737126a52b2649cb6562cc87a19d7
HoneySafonovets <kfs.boi@yandex.ru>
2025-01-08 15:28:28 +0300

    feat: add function getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -501,8 +501,17 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  let count = 0;
+  arr.map((e) => {
+    if (!e) {
+      count += 1;
+      return e;
+    }
+    return e;
+  });
+
+  return count;
 }
 
 /**

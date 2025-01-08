861f162bc9bba7c4a9329b5dc542a646cf38a88d
HoneySafonovets <kfs.boi@yandex.ru>
2025-01-08 19:51:34 +0300

    feat: add function propagateItemsByPositionIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -680,8 +680,17 @@ function findLongestIncreasingSubsequence(nums) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  if (arr.length === 0) return [];
+
+  const newArr = [];
+
+  arr.map((e, i) => {
+    newArr.push(Array(i + 1).fill(e));
+    return e;
+  });
+
+  return newArr.flat(10);
 }
 
 /**
